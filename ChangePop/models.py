38e6b4f70f91ebb5e821aff06d0e8252a400b99e
kotopulaki <alvaro2garcia.dobon@gmail.com>
2019-04-05 18:21:16 +0200

    Models.py ya esta acabado, una vez levantada la bd, ya estaria el tema de la bd ready para meterle consultas y operaciones rollo api

--- ChangePop/models.py
+++ ChangePop/models.py
@@ -52,7 +52,7 @@ class Users(UserMixin, db.Model):
 
 
 class Products(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True, nullable=False, autoincrement=True)
     tittle = db.Column(db.String(255), unique=False, index=True, nullable=False)
     descript = db.Column(db.String(255), unique=False, nullable=False)
     price = db.Column(db.Float, unique=False, nullable=False)
@@ -60,16 +60,15 @@ class Products(db.Model):
     ban_reason = db.Column(db.String(255), unique=False, nullable=False)
     bid_dat = db.Column(db.Date, unique=False, nullable=True)
     num_visits = db.Column(db.Integer, unique=False, nullable=False)
-    boost_date
-    followers
-    is_removed
-    place
-    user_id
-    ts_edit
+    boost_date = db.Column(db.Date, unique=False, nullable=True)
+    followers = db.Column(db.Integer, unique=False, nullable=False)
+    is_removed = db.Column(db.Boolean, unique=False, nullable=False)
+    place = db.Column(db.String(255), unique=False, nullable=False)
+    ts_edit = db.Column(db.DateTime, unique=False, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
     def __repr__(self):
-        return '{},{},{}'.format(self.nia, self.carrera, self.privilegiado)
+        return '{},{},{},{}'.format(self.id, self.tittle, self.user_id)
 
 
 class Images(db.Model):
@@ -79,116 +78,113 @@ class Images(db.Model):
     def __repr__(self):
         return '{},{}'.format(self.products_id, self.image_url)
 
-class Reto(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(50), nullable=False)
-    descripcion = db.Column(db.String(10000), nullable=False)
-    url = db.Column(db.String(200), nullable=False)
-    pregunta = db.Column(db.String(500), nullable=False)
-    activo = db.Column(db.Boolean, nullable=False)
-    fecha_inicio = db.Column(db.DateTime, index=True, nullable=True)
-    fecha_fin = db.Column(db.DateTime, index=True, nullable=True)
+
+class Reports(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True, index=True, nullable=False)
+    reason = db.Column(db.String(255), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'))
+    report_date = db.Column(db.DateTime, index=True, unique=False, nullable=True)
 
     def __repr__(self):
-        return '{}, {}, {}, {}, {}'.format(self.titulo, self.descripcion, self.pregunta, self.fecha_inicio, self.fecha_fin)
-
-    def activate(self):
-        self.activo=True
-
-    def desactivate(self):
-        self.activo=False
-
-    def calcularStats(reto_ident):
-        aciertos = 0
-        fallos = 0
-        blanco = 0
-        total = 0
-        lista = Respuesta.obtenerrespuestas(reto_ident)
-        for elemento in lista:
-            resp = [elemento.respuesta1, elemento.respuesta2, elemento.respuesta3, elemento.respuesta4]
-            i = 0
-            while i < 4:
-                total = total + 1
-                if resp[i] == 0:
-                    blanco = blanco+1
-                elif resp[i] == 1:
-                    aciertos = aciertos+1
-                else:
-                    fallos = fallos+1
-                i = i+1
-        if total == 0:
-            aciertos = 0
-            fallos = 0
-            blanco = 0
-        else:
-            aciertos = int((aciertos * 100) / total)
-            fallos = int((fallos * 100) / total)
-            blanco = int((blanco * 100) / total)
-        return [aciertos, fallos, blanco, total]
+        return '{},{},{},{}'.format(self.id, self.reason, self.user_id, self.product_id)
 
 
-
-class Opciones(db.Model):
-    reto_id = db.Column(db.Integer, db.ForeignKey('reto.id'), primary_key=True)
-    cuerpo = db.Column(db.String(500), nullable=False, primary_key=True)
-    verdadero = db.Column(db.Boolean, index=True, nullable=False)
+class Payments(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True, index=True, nullable=False)
+    pay_date = db.Column(db.DateTime, index=True, unique=False, nullable=False)
+    amount = db.Column(db.Float, unique=False, nullable=False)
+    iban = db.Column(db.String(255), unique=False, nullable=False)
+    boost_date = db.Column(db.DateTime, unique=False, nullable=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'))
 
     def __repr__(self):
-        return '{}'.format(self.cuerpo)
+        return '{},{},{}'.format(self.id, self.amount, self.product_id)
 
-class Responde(db.Model):
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), primary_key=True)
-    respuesta_id = db.Column(db.Integer, db.ForeignKey('respuesta.id'), primary_key=True)
 
-    def __repr__(self):
-        return '{}'.format(self.respuesta)
-
-class Publica(db.Model):
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.email'), primary_key=True)
-    reto_id = db.Column(db.Integer, db.ForeignKey('reto.id'), primary_key=True)
-    ganador = db.Column(db.Boolean, nullable=False)
+class Coments(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True, index=True, nullable=False)
+    publish_date = db.Column(db.DateTime, index=True, unique=False, nullable=False)
+    body = db.Column(db.String(255), unique=False, nullable=False)
+    user_to = db.Column(db.Integer, db.ForeignKey('Users.id'))
+    user_from = db.Column(db.Integer, db.ForeignKey('Users.id'))
 
     def __repr__(self):
-        return '{}'.format(self.ganador)
+        return '{},{},{},{}'.format(self.id, self.body, self.user_to, self.user_from)
 
-class Respuesta(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    reto_id = db.Column(db.Integer, db.ForeignKey('reto.id'))
-    respuesta1 = db.Column(db.Integer)# No contestado=0, Acierto=1, Fallo=2
-    respuesta2 = db.Column(db.Integer)
-    respuesta3 = db.Column(db.Integer)
-    respuesta4 = db.Column(db.Integer)
-    def __repr__(self):
-        return '{}''{}''{}''{}'.format(self.respuesta1, self.respuesta2, self.respuesta3, self.respuesta4)
-        #return [str(self.respuesta1), str(self.respuesta2), str(self.respuesta3), str(self.respuesta4)]
 
-    def obtenerrespuestas(iden):
-        # SELECT cuerpo,verdadero FROM Opciones WHERE Opciones.reto_id=id
-        return Respuesta.query.filter_by(reto_id=iden).all()
-
-class Admin(db.Model):
-    email = db.Column(db.String(120), primary_key=True, index=True, unique=True, nullable=False)
-    activado= db.Column(db.Boolean)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def activate(self):
-        self.activado=True
-
-    def is_activated(self):
-        return (self.activado==True)
+class CatProducts(db.Model):
+    cat_name = db.Column(db.Integer, db.ForeignKey('Categories.cat_name'), primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), primary_key=True)
 
     def __repr__(self):
-        return '{},{}'.format(self.email,self.activado)
+        return '{},{}'.format(self.cat_name, self.product_id)
+
+
+class Interest(db.Model):
+    cat_name = db.Column(db.Integer, db.ForeignKey('Categories.cat_name'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+
+    def __repr__(self):
+        return '{},{}'.format(self.cat_name, self.user_id)
+
+
+class Follows(db.Model):
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+
+    def __repr__(self):
+        return '{},{}'.format(self.product_id, self.user_id)
+
+
+class Bids(db.Model):
+    bid = db.Column(db.Float, unique=False, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    ts_create = db.Column(db.DateTime, unique=False, nullable=True)
+    ts_create = db.Column(db.DateTime, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '{},{},{}'.format(self.product_id, self.user_id, self.bid)
+
+
+class Trades(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True, index=True, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'))
+    user_sell = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_buy = db.Column(db.Integer, db.ForeignKey('User.id'))
+    closed = db.Column(db.Boolean, unique=False, nullable=False)
+    price = db.Column(db.Float, unique=False, nullable=False)
+    ts_create = db.Column(db.DateTime, unique=False, nullable=True)
+    ts_create = db.Column(db.DateTime, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '{},{},{},{},{}'.format(self.id, self.user_sell, self.user_buy, self.product_id, self.price)
+
+
+class Messages(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True, index=True, nullable=False)
+    trade_id = db.Column(db.Integer, db.ForeignKey('Trades.id'))
+    user_to = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user_from = db.Column(db.Integer, db.ForeignKey('User.id'))
+    body = db.Column(db.String(255), unique=False, nullable=False)
+    msg_date = db.Column(db.DateTime, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '{},{},{},{},{}'.format(self.id, self.user_to, self.user_from, self.trade_id, self.body)
+
+
+class TradesOffers(db.Model):
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), primary_key=True)
+    trade_id = db.Column(db.Integer, db.ForeignKey('Trades.id'), primary_key=True)
+
+    def __repr__(self):
+        return '{},{}'.format(self.product_id, self.trade_id)
+
 
 @login.user_loader
 def load_user(id):
-    return Usuario.query.get(int(id))
+    return Users.query.get(int(id))
 
 
 
