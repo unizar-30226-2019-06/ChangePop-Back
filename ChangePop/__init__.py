0299fef6f58fb5dcdf5bbf9ebce15814e1401633
kotopulaki <alvaro2garcia.dobon@gmail.com>
2019-04-05 16:34:49 +0200

    importo las librerias necesarias para la database

--- ChangePop/__init__.py
+++ ChangePop/__init__.py
@@ -1,8 +1,29 @@
 from flask import Flask, url_for, render_template
 
 
+
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_wtf import CsrfProtect
+import sys
+
+
+
+from app import routes, models
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_object(Config)
+    db = SQLAlchemy(app)
+    db.drop_all()
+    db.create_all()
+    migrate = Migrate(app, db)
+    login = LoginManager(app)
+    login.login_view = 'login'
+    CsrfProtect(app)
 
     from ChangePop import user
     app.register_blueprint(user.bp)
