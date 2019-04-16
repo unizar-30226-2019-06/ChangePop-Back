ffabb2cadf37f44a4a23b51cd764554a12548741
JaviBite <javiergimenezgarces@gmail.com>
2019-04-16 14:42:04 +0200

    Try deploy azure Def Dummy

--- application.py
+++ application.py
@@ -1,10 +1,6 @@
-import ChangePop
+from flask import Flask
+app = Flask(__name__)
 
-
-#  We have to disale this i pyllint, because pylint will fail our build every
-#  time it encounters this "global" variable
-
-app = ChangePop.get_app()
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def hello():
+    return "Hello World!"
