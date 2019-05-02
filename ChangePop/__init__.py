c8c6af5034a34a1611d6a21f1b586881901a6127
JaviBite <javiergimenezgarces@gmail.com>
2019-05-02 13:47:34 +0200

    Fix sessions

--- ChangePop/__init__.py
+++ ChangePop/__init__.py
@@ -18,6 +18,24 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
+
+from flask import g
+from flask.sessions import SecureCookieSessionInterface
+from flask_login import user_loaded_from_header
+
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    """Prevent creating session from API requests."""
+    def save_session(self, *args, **kwargs):
+        if g.get('login_via_header'):
+            return
+        return super(CustomSessionInterface, self).save_session(*args,
+                                                                **kwargs)
+
+
+app.session_interface = CustomSessionInterface()
+
+
 from ChangePop.exeptions import NotLoggedIn
 login.unauthorized_handler(NotLoggedIn.not_auth_handler)
 
